897532d892f352106473e6c43ee67d0548a9f3db
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2024-10-06 11:28:50 +0500

    Created Layouts

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Navbar :routes="routes"/>
+        <NuxtPage />
+    </div>
+</template>
+    
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue";
+import type Route from '~/types/route';
+import { useStore } from 'vuex';
+const store = useStore();
+const routes: Route[] = reactive([
+  { label: 'Home', href:'/', active:true },
+  { label: 'Events', href:'/events', active: false },
+  { label: 'Login', href:'login', active: false}
+]);
+  if(store.getters.getUserToken()){
+    routes.splice(routes.findIndex((route)=>route.label === "Login"),1)
+}
+
+</script>
+
+<style scoped>
+
+</style>
