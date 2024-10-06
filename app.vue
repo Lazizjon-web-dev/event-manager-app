efae2e0b5832ab57fdb5b6ca0c6e6645dbf92193
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2024-10-06 11:31:57 +0500

    Created checking for token when app loaded

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout />
   </div>
 </template>
+<script lang="ts" setup>
+import { useLocalStorage } from '@vueuse/core';
+import { useStore } from 'vuex';
+const store = useStore();
+const storedToken = useLocalStorage('userToken', '');
+if (storedToken.value) {
+  store.commit('setUserToken',storedToken)
+}
+</script>
+
