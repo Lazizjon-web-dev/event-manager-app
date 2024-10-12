02d12d89da3f6e0f1f41ee5cca8245ab432fd0ec
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2024-10-12 19:37:29 +0500

    Changed default length of shortenedDescription

--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -1,4 +1,4 @@
-export function shortenDescription(longString: string,length=100,start=0): string {
+export function shortenDescription(longString: string,length=85,start=0): string {
     if (longString.length <= length) return longString;
     
     return longString.slice(start, length) + '...';
